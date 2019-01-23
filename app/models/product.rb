77224189a0625d183c2870f098c90756d8e34e14
JackMarx <seriousfools@gmail.com>
2019-01-23 10:31:00 -0600

    happy/sad path

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: {maximum: 254}
+  
+  validates :price, presence: true
+  validates :price, numericality: {less_than: 100_000_000, greater_than: 0}
+
+  validates :description, presence: true
+  validates :description, length: {in: 10..300}
+
   def is_discounted?
     price < 10
   end
