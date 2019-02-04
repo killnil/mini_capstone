a4e3970df95c79d7fca4691624e849a7bc5a7925
JackMarx <seriousfools@gmail.com>
2019-02-04 12:31:29 -0600

    adding multiple products per order

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,8 @@ Rails.application.routes.draw do
     get '/orders' => 'orders#index'
     post '/orders' => 'orders#create'
     get '/orders/:id' => 'orders#show'
+
+    get '/carted_products' => 'carted_products#index'
+    post '/carted_products' => 'carted_products#create'
   end
 end
