fd35ad945319067f2ee2f2e3e472890db0e700af
JackMarx <seriousfools@gmail.com>
2019-02-05 11:10:39 -0600

    order model methods

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,31 +7,14 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    carted_products = current_user.cart
-    subtotal_sum = 0
-
-    carted_products.each do |carted_product|
-      subtotal_sum += carted_product.product.price * carted_product.quantity
-    end
-
-    calculated_tax = subtotal_sum * 0.09
-    calculated_total = subtotal_sum + calculated_tax
-
-
-    @order = Order.new(
-                       user_id: current_user.id,
-                       subtotal: subtotal_sum,
-                       tax: calculated_tax,
-                       total: calculated_total
-                      )
+    @order = Order.new( user_id: current_user.id )
 
     @order.save
-
-    carted_products.each do |carted_product|
-      carted_product.status = "purchased"
-      carted_product.order_id = @order.id
-      carted_product.save
-    end
+    @order.associate_cart
+    @order.calculate_subtotal
+    @order.calculate_tax
+    @order.calculate_total
+    @order.save
 
     render 'show.json.jbuilder'
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,21 +3,35 @@ class Order < ApplicationRecord
   has_many :carted_products
   has_many :products, through: :carted_products
 
-  # def calculate_subtotal
-  #   self.subtotal = quantity * product.price
+  # def initialize(input_options)
+  #   super
+  #   calculate_totals
   # end
 
-  # def calculate_tax
-  #   self.tax = subtotal * 0.09
-  # end
+  def calculate_subtotal
+    self.subtotal = carted_products.sum {|carted_product| carted_product.subtotal }
+  end
 
-  # def calculate_total
-  #   self.total = tax + subtotal
-  # end
+  def calculate_tax
+    self.tax = subtotal * 0.09
+  end
+
+  def calculate_total
+    self.total = subtotal + tax
+  end
+
+  def associate_cart
+    user.cart.each do |carted_product| 
+      carted_product.update(status: "purchased", order_id: id)
+    end
+  end
 
   # def calculate_totals
+  #   save
+  #   associate_cart
   #   calculate_subtotal
   #   calculate_tax
   #   calculate_total
+  #   save
   # end
 end
